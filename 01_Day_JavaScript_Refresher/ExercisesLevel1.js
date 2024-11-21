a2ad23a51b3d75fc271ff18283448bc4957a63dc
Yabe12 <yeabsirabehailu92@gmail.com>
2024-11-21 23:03:52 +0300

    done the 4 challenge of exercise 1

--- 01_Day_JavaScript_Refresher/ExercisesLevel1.js
+++ 01_Day_JavaScript_Refresher/ExercisesLevel1.js
@@ -1,8 +1,18 @@
+
+
+
 // Declare an empty array;
-// Declare an array with more than 5 number of elements
-// Find the length of your array
 const techEnd =Array();
+
+
+// Declare an array with more than 5 number of elements
 const num1 =Array(1,2,3,4,5,6,7,8,9);
 console.log(num1.length);
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Find the length of your array
 console.log(nums.length); 
+
+// Get the first item, the middle item and the last item of the array
+let mid =Math.floor(nums.length/2);
+let last =nums.length-1;
+console.log(nums[0],nums[mid],nums[last]);
