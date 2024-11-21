ee0861adb9ac71f6ec70f63535e12f3868b81356
Yabe12 <yeabsirabehailu92@gmail.com>
2024-11-21 22:47:17 +0300

    finish the first 3 qut

---
+++ 01_Day_JavaScript_Refresher/ExercisesLevel1.js
@@ -0,0 +1,8 @@
+// Declare an empty array;
+// Declare an array with more than 5 number of elements
+// Find the length of your array
+const techEnd =Array();
+const num1 =Array(1,2,3,4,5,6,7,8,9);
+console.log(num1.length);
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(nums.length); 
