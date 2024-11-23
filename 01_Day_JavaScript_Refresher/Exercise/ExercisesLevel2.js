4beda8025eb0687ea2d1dd76614aac083a83b839
Yabe12 <yeabsirabehailu92@gmail.com>
2024-11-23 19:54:22 +0300

    complete the level 2 of exercise one

--- 01_Day_JavaScript_Refresher/Exercise/ExercisesLevel2.js
+++ 01_Day_JavaScript_Refresher/Exercise/ExercisesLevel2.js
@@ -54,3 +54,17 @@ console.log(countries);
 // In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
 
 console.log(web_techs);
+
+// Concatenate the following two variables and store it in a fullStack variable.
+
+// const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+// const backEnd = ['Node', 'Express', 'MongoDB']
+
+// console.log(fullStack)
+// ["HTML", "CSS", "JS", "React", "Redux", "Node", "Expres
+
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node', 'Express', 'MongoDB']
+
+const fullStack = frontEnd.concat(backEnd);
+console.log(fullStack);
